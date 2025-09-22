60da902cb3be910a906aa675cf8009d56a77ad65
TheBlue1026 <23730092@ms.uit.edu.vn>
2025-09-22 19:51:40 +0700

    Initial commit for the project

---
+++ src/main/java/com/uit/project/quanlyhocphiuit/QuanLyHocPhiUIT.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.uit.project.quanlyhocphiuit;
+
+/**
+ *
+ * @author thebl
+ */
+public class QuanLyHocPhiUIT {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
